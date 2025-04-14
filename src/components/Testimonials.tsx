f6da86908e8f397dc61b03cb9f9fe4e1ed3edbda
Dilanplayz <102540218+DilanTheDev@users.noreply.github.com>
2025-04-14 16:02:54 +0100

    Update Testimonials.tsx

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -37,6 +37,20 @@ const testimonials: Testimonial[] = [
     title: "Amazing",
     text: "This is by far the best server hosting company I have ever used. They have AMAZING support and good prices. I've been using Cherry hosting for a couple of..."
   }
+  {
+    name: "Trustpilot Reviewer",
+    date: "11th of October 2024",
+    rating: 5,
+    title: "Amazing",
+    text: "This is by far the best server hosting company I have ever used. They have AMAZING support and good prices. I've been using Cherry hosting for a couple of..."
+  }
+  {
+    name: "Trustpilot Reviewer",
+    date: "11th of October 2024",
+    rating: 5,
+    title: "Amazing",
+    text: "This is by far the best server hosting company I have ever used. They have AMAZING support and good prices. I've been using Cherry hosting for a couple of..."
+  }
 ];
 
 const TestimonialCard = ({ testimonial }: { testimonial: Testimonial }) => {
